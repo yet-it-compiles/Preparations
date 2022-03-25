ef367acb7fb4de22e6fd20f29b78e8129b882cbc
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-03-25 12:08:16 -0700

    Interview Question 1.3
    Write a method to replace all spaces in a string with '%20'. You may assume that the string 
    has sufficient space at the end to hold the additional characters, and that you are given the "true" 
    length of the string.

---
+++ Cracking The Coding Interview/Chapter_1_URLify.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * Write a method to replace all spaces in a string with '%20'. You may assume that the string
+ * has sufficient space at the end to hold the additional characters, and that you are given the "true"
+ * length of the string.
+ */
+public class Chapter_1_URLify {
+    public static void main(String[] args) {
+        String firstCase = "Mr John Smith";
+
+        String result = URLify(firstCase);
+        System.out.println("The returned string result is: " + result);
+    }
+
+
+    public static String URLify(String firstCase) {
+        firstCase = firstCase.replace(" ", "%20");
+        return firstCase;
+    }
+}
+
