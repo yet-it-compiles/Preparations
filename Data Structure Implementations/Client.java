7b85f6ff6f5b733126706687fada738de7376f90
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-03-26 11:05:09 -0700

    Initial commit
    Client file where initializations, and demonstrations of future implemented data structures will be used.

---
+++ Data Structure Implementations/Client.java
@@ -0,0 +1,14 @@
+package package3.DataStructureImplementations;
+
+import java.util.ArrayList;
+
+public class Client {
+    public static void main(String[] args) {
+        DynamicArray myDynamicArray = new DynamicArray();
+        System.out.println(myDynamicArray.sizeOfArray);
+        myDynamicArray.add("Tyler");
+        System.out.println(myDynamicArray);
+
+        ArrayList mine = new ArrayList();
+    }
+}
