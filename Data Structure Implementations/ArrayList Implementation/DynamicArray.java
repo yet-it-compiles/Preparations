6f27b2837e9bb4de69ffa4cfabe0eda291f09787
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-04-12 12:45:09 -0700

    Implemented .add(), .size()
    Dynamic array may now add elements to itself, along with being able to return its size.

--- Data Structure Implementations/ArrayList Implementation/DynamicArray.java
+++ Data Structure Implementations/ArrayList Implementation/DynamicArray.java
@@ -8,11 +8,29 @@ public class DynamicArray {
     private final int DEFAULT_CAPACITY = 10;
     private int sizeOfArray = 0;
 
-    public DynamicArray(){this.underlyingArray = new Object[DEFAULT_CAPACITY];}
-
-    public void add(Object newElement){
-        this.underlyingArray[this.sizeOfArray] = newElement;
-        sizeOfArray++;
+    /**
+     * Initializes the Dynamic Array to its default capacity of 10
+     */
+    public DynamicArray() {
+        this.underlyingArray = new Object[DEFAULT_CAPACITY];
     }
 
+    /**
+     * Adds an element to the first available space in the array
+     *
+     * @param newElement the element the user wants to add
+     */
+    public void add(Object newElement) {
+        if (sizeOfArray == underlyingArray.length) // Checks to see if the array has to be increased
+
+            this.underlyingArray[this.sizeOfArray++] = newElement; // Adds item to the next available space
+    }
+
+    /**
+     * Returns the size of the array
+     */
+    public int size() {
+        return this.sizeOfArray;
+    }
+    
 }
